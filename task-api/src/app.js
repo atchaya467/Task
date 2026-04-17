02a39512edc66e6ed916cefd012428f7bb9e5f51
Atchaya VS <anithaseenivasan246@gmail.com>
2026-04-17 10:02:00 +0530

    Final Polish: Fix absolute paths for Vercel, sync dependencies, and clean up repository

--- task-api/src/app.js
+++ task-api/src/app.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const express = require('express');
 const taskRoutes = require('./routes/tasks');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use('/tasks', taskRoutes);
 
 app.use((err, req, res, next) => {
